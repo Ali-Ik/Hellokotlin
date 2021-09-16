0c9387a620d6b61ecc59da9d990b6c7434841762
Ali-Ik <37378601+Ali-Ik@users.noreply.github.com>
2021-09-16 18:03:14 +0100

    first commit

---
+++ hello.kt
@@ -0,0 +1,32 @@
+fun main(args:Array<String>) {
+    //Print statements
+    //Println escapes to a new line
+    println("Hello, Kotlin!")
+    //Print remains on the line
+    println("I am Kotlin ")
+
+    //Addition
+    print(96+10)       
+    
+    //Math Functions
+    val myAge: Int=24
+    println(myAge)
+
+    var averageAge: Double=24.0
+    averageAge=((24.0+30.0)/2)
+    println(averageAge)
+
+    var testNumber: Int=19
+    val evenOdd: Int=testNumber%2
+    testNumber+=1
+    println(evenOdd)
+    println(testNumber)
+
+    var answer:Int=0
+    answer+=1
+    println(answer)
+    answer+=10
+    println(answer)
+    answer shl 1
+    println(answer)
+}
