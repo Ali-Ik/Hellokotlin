44bc2d4123b622a5bbfb52ed97bae49740b21681
Ali-Ik <37378601+Ali-Ik@users.noreply.github.com>
2021-09-17 21:09:50 +0100

    Step 1

--- hello.kt
+++ hello.kt
@@ -1,32 +1,15 @@
-fun main(args:Array<String>) {
+fun main() {
     //Print statements
     //Println escapes to a new line
     println("Hello, Kotlin!")
     //Print remains on the line
     println("I am Kotlin ")
 
-    //Addition
-    print(96+10)       
+    //Arithmetic operations
+    println((9+11*(6-2)/5)%1)
     
-    //Math Functions
-    val myAge: Int=24
-    println(myAge)
-
-    var averageAge: Double=24.0
-    averageAge=((24.0+30.0)/2)
-    println(averageAge)
-
-    var testNumber: Int=19
-    val evenOdd: Int=testNumber%2
-    testNumber+=1
-    println(evenOdd)
-    println(testNumber)
-
-    var answer:Int=0
-    answer+=1
-    println(answer)
-    answer+=10
-    println(answer)
-    answer shl 1
-    println(answer)
+    //operator assignment
+    var money: Int=1300
+    money*=2
+    println(money)
 }
---
+++ hello0.kt
@@ -0,0 +1,27 @@
+fun main(){
+    //Math Functions
+val myAge: Int=24
+println(myAge)
+
+var averageAge: Double=24.0
+averageAge=((24.0+30.0)/2)
+println(averageAge)
+
+println()
+
+var testNumber: Int=9
+val evenOdd: Int=testNumber%2
+println(evenOdd)
+testNumber+=1
+println(testNumber)
+
+println()
+
+var answer:Int=0
+answer+=1
+println(answer)
+answer+=5
+println(answer)
+val new_answer: Int = (answer shl 2)
+println(new_answer)
+}
